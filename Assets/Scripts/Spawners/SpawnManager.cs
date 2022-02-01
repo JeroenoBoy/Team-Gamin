272677f8c5916f989a8bc075bbfb3599281df090
Joep-vink <78424962+Joep-vink@users.noreply.github.com>
2022-02-01 10:39:00 +0100

    setup

---
+++ Assets/Scripts/Spawners/SpawnManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [Range(0.1f, 2f)]
+    public float timeBetweenCardSpawn;
+}
