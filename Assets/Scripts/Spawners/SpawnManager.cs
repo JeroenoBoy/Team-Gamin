39cdfb3127436441283ed4cff519fb4b40b491c6
Joep-vink <78424962+Joep-vink@users.noreply.github.com>
2022-02-03 13:12:40 +0100

    Castle finnished

--- Assets/Scripts/Spawners/SpawnManager.cs
+++ Assets/Scripts/Spawners/SpawnManager.cs
@@ -7,8 +7,6 @@ public class SpawnManager : ObjectPool
     [Range(0.1f, 2f)]
     public float timeBetweenSpawn;
 
-    public Transform objToSpawn;
-
     private void Start()
     {
         StartCoroutine(WaveSpawner());
