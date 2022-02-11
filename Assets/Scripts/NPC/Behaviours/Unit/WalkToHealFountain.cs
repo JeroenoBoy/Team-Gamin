65ef07d766cdc2c12a27fb9a9895e16fca8f8296
JeroenoBoy <Jeroen@vandegeest.eu>
2022-02-11 15:28:01 +0100

    IDK

---
+++ Assets/Scripts/NPC/Behaviours/Unit/WalkToHealFountain.cs
@@ -0,0 +1,11 @@
+﻿namespace NPC.Behaviours.Unit
+{
+    public class WalkToHealFountain : AIBehavior
+    {
+        public override void PhysicsUpdate()
+        {
+            var position = HealingFountain.instance.transform.position;
+            movement.AddForce(MoveWithArrive(position));
+        }
+    }
+}
