4d3807d06315a48d22141b19f9ead53aa676ed3a
JariPD <200502@student.glu.nl>
2022-02-14 13:06:02 +0100

    highlight what behaviour is selected

--- Assets/Scripts/UI/BehaviourMenu.cs
+++ Assets/Scripts/UI/BehaviourMenu.cs
@@ -1,16 +1,32 @@
 using UnityEngine;
+using System;
 using NPC.UnitData;
+using UnityEngine.UI;
 
 public class BehaviourMenu : MonoBehaviour
 {
-    //private UnitState settings => FindObjectOfType<UnitState>();
     public UnitState unitState;
+    public Button[] Button;
+    private int index;
+
+    private void Start()
+    {
+        Button[0].image.color = Color.red;
+    }
 
     public void SetBehaviour(string unit)
     {
+        for (int i = 0; i < Button.Length; i++)
+        {
+            Button[i].image.color = Color.black;
+        }
+
         if (UnitState.TryParse(unit, out UnitState state))
             unitState = state;
         else
             Debug.LogError("Invalid State!");
+
+        index = Array.IndexOf(UnitStateExtensions.states, unitState);
+        Button[index].image.color = Color.red;
     }
 }
