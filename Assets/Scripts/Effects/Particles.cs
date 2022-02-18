8659cb55d89b95405242e10ed75221a37361462e
JeroenoBoy <Jeroen@vandegeest.eu>
2022-02-18 14:03:10 +0100

    Changed Effects

--- Assets/Scripts/Effects/Particles.cs
+++ Assets/Scripts/Effects/Particles.cs
@@ -2,11 +2,11 @@ using UnityEngine;
 
 public class Particles : MonoBehaviour
 {
-    public Vector3 speed; 
+    public Vector3 Speed; 
 
 
     void Update()
     {
-        transform.Rotate(speed * Time.deltaTime);
+        transform.Rotate(Speed * Time.deltaTime);
     }
 }
